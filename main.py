285c4f589725a057f0c1be2d2e78bb2c5622e1d5
Wilber Alegria <52012885+XwilberX@users.noreply.github.com>
2024-12-22 01:36:39 -0600

    add basic handle_exception

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+# Python imports
+
+# Libraries imports
 from robyn import Robyn
+from robyn.exceptions import HTTPException
 
-# config
-from config.config import settings
-
-# apps
+# Project imports
 from src.auth.router import router as auth_router
 from src.blogs.router import router as blogs_router
+from config.config import settings
+
 
 app = Robyn(__file__)
 
 
+@app.exception
+def handle_exception(error):
+    return {
+        "status": "error",
+        "status_code": error.status_code,
+        "detail": error.detail,
+    }
+
+
 @app.get("/")
 async def h(request):
-    return "Hello, world"
+    raise HTTPException(status_code=500, detail="Internal server error")
 
 
 app.include_router(router=auth_router)
